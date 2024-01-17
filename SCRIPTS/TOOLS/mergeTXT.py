08a3782303d7cb237f46fecc867ed22a25acd31b
Mehdi <mehdi.nedjar60@gmail.com>
2024-01-17 14:21:57 +0100

    First commit

---
+++ SCRIPTS/TOOLS/mergeTXT.py
@@ -0,0 +1,53 @@
+import os
+import sys
+
+# Asks the user to specify the path to the folder containing the .txt files to be merged
+folder = input('Please specify the path of the folder containing the .txt files to be merged: ')
+
+# Checks if the folder exists
+if not os.path.isdir(folder):
+    print(f'The specified folder ({folder}) does not exist or isn\'t accessible.')
+    exit()
+
+# Defines the name of the output file
+output_file = input('Please specify the desired name of the output file (without .txt): ')
+output_file = output_file + '.txt'
+
+# Counts the number of merged files
+nb_merged_files = 0
+
+# Opens the output file in write mode
+with open(output_file, 'w') as f_out:
+
+    # Browses all files in the folder and its subfolders
+    for current_directory, subdirectories, files in os.walk(folder):
+
+        # Browses all files in the current directory
+        for file in files:
+
+            # Checks that the file is a .txt file
+            if file.endswith('.txt') and file != output_file:
+
+                # Opens file in read mode
+                with open(os.path.join(current_directory, file), 'r') as f_in:
+
+                    # Reads file content
+                    content = f_in.read()
+
+                    # Writes file content to output file
+                    f_out.write(content)
+					
+					# Adds a newline after each file's content
+                    f_out.write('\n')
+
+                    # Increments the number of merged files
+                    nb_merged_files += 1
+
+# Displays a confirmation message
+if nb_merged_files > 0:
+    print(f'{nb_merged_files} files have been successfully merged into {output_file}.')
+else:
+    print(f'No .txt files were found in the {folder} folder.')
+
+# Waits for the user to press a key to close the console window
+input('Press a key to close the window...')
